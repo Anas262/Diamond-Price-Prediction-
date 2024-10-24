8ff930a9692009d4db81675dab56b3aaaede91a8
Anas262 <khananas82732@gmail.com>
2024-10-24 17:40:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 from src.pipelines.prediction_pipelines import CustomData, PredictPipeline
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html")  # Home page route
 
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict_datapoint():
     if request.method == "GET":
-        return render_template("form.html")
+        return render_template("form.html")  # Render the form on GET request
 
     else:
+        # Collect data from the form
         data = CustomData(
             carat=float(request.form.get("carat")),
             depth=float(request.form.get("depth")),
@@ -27,12 +27,16 @@ def predict_datapoint():
             color=request.form.get("color"),
             clarity=request.form.get("clarity"),
         )
+
+        # Prepare the data for prediction
         final_new_data = data.get_data_as_dataframe()
         predict_pipeline = PredictPipeline()
         pred = predict_pipeline.predict(final_new_data)
 
+        # Round the prediction result
         results = round(pred[0], 2)
 
+        # Render the form page again with the prediction result
         return render_template("form.html", final_result=results)
 
 
